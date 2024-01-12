20de982ae7ef94330d3846c3ce4cb90c22d5c5bd
Ruslan Shkarin <ruslanusd@gmail.com>
2024-01-12 11:48:56 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get('/api/v1/info', (req, res) => {
+  res.send({ application: 'sample-app', version: '1' });
+});
+app.post('/api/v1/getback', (req, res) => {
+  res.send({ ...req.body });
+});
+app.listen(80, () => console.log(`Listening on: 80`));
